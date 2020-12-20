3dec8913c2dc1cba01b210158016fa5302c06b2f
Daniel G. Machado <daniel.g_machado@hotmail.com>
2020-12-19 22:07:29 -0300

    Classe Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Frases do dia",
-    home: Container(
-      color: Colors.white,
-      child: Column(
-        children: <Widget>[ 
-          Text(
-          "Texto 1 fgf fg    fgff gf", 
-            style: TextStyle(
-              fontSize: 25
-            )),
-        Text("Texto 2"),
-        Text("Texto 3"),
-    ]
-      )
+      home: Scaffold(
+    appBar: AppBar(title: Text("FrasesApp"), backgroundColor: Colors.green),
+    body: Padding(
+      padding: EdgeInsets.all(16),
+      child: Text("Conteúdo"),
+    ),
+    bottomNavigationBar: BottomAppBar(
+      color: Colors.lightGreen,
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Row(
+          children: <Widget>[Text("Conteúdo 1"), Text("Conteúdo 2")],
+        ),
       ),
-  ));
+    ),
+  )
+      //debugShowCheckedModeBanner: false,
+      //title: "Frases do dia",
+      /*home: Container(
+        //color: Colors.white,
+        decoration:
+            BoxDecoration(border: Border.all(width: 3, color: Colors.white)),
+        child: Column(
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              //TextField(),
+              Text("Texto 2"),
+              Text("Texto 3"),
+              FlatButton(
+                  onPressed: () {
+                    print("Pressionado");
+                  },
+                  color: Colors.blue,
+                  child: Text("Ok",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          decoration: TextDecoration.none)))
+            ])),
+            */
+      ));
 }
